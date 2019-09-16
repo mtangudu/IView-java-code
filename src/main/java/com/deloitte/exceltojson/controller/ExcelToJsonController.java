e46c24c402af4f36233b68031665b67e870c8f0f
Manikanta <mtangudu@deloitte.com>
2019-09-16 13:23:05 +0530

    added rename and save As service.

--- src/main/java/com/deloitte/exceltojson/controller/ExcelToJsonController.java
+++ src/main/java/com/deloitte/exceltojson/controller/ExcelToJsonController.java
@@ -253,7 +253,117 @@ public class ExcelToJsonController {
 		return JSONObject.quote("Successfully uploaded file : " + file.getOriginalFilename());
 	}
 
-	@CrossOrigin(origins = "http://localhost:4200")
+	@CrossOrigin(origins = "*")
+	@PostMapping("/saveAs")
+	public ResponseEntity<String> saveAsData(@RequestParam("nodedata") NodeData data, @RequestParam("description") String id) throws IOException {
+		log.info("uploading the file " + id);
+		log.info("File passed : " + id.toString());	
+		
+		String serviceLine = id;
+		Properties fieldDetails = getFieldPasroperties();
+		ObjectMapper mapper = new ObjectMapper();
+
+		if (!fieldDetails.isEmpty() && id.length()>0 ) {
+			InputStream propertiesInput = ExcelToJsonController.class.getClassLoader()
+					.getResourceAsStream("application.properties");
+			Properties appProperties = new Properties();			
+			appProperties.load(propertiesInput);
+			MongoClient mongoClient = new MongoClient(appProperties.getProperty("mongodb.host"),new Integer(appProperties.getProperty("mongodb.port")));
+			MongoDatabase db = mongoClient.getDatabase(appProperties.getProperty("mongodb.db"));
+
+			MongoCollection<Document> coll = db.getCollection("service_line");
+			BasicDBObject whereQuery = new BasicDBObject();
+			whereQuery.put("_id",serviceLine);
+			FindIterable<Document> cursor = coll.find(whereQuery);
+			
+			if(cursor.first() == null) {
+				//new file
+				Document doc = new Document();
+				doc.append("_id", serviceLine);
+				doc.append("description", serviceLine);
+				coll.insertOne(doc);
+				
+				coll = db.getCollection(appProperties.getProperty("mongodb.collection"));
+				
+				HashMap<String, Object> metaData = new HashMap<String, Object>();
+				
+				if (coll.find().first().get("meta") != null) 
+					metaData = (HashMap<String, Object>) coll.find().first().get("meta");
+				
+				doc = new Document();
+				doc.append("_id", serviceLine);
+				doc.append("meta", metaData);
+				doc.append("data", mapper.convertValue(data, Map.class));
+
+				coll.insertOne(doc);
+				mongoClient.close();
+				
+			}else {
+				mongoClient.close();
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Duplicate id. Please try again with new id.");
+			}
+			
+		}
+		return ResponseEntity.ok().body(JSONObject.quote("Successfully added file : " + id));
+	}
+	
+    @CrossOrigin(origins = "*")
+    @GetMapping("/rename")
+    public ResponseEntity<String> rename(@RequestParam("oldId") String oldId, @RequestParam("newId") String newId) throws IOException {
+		
+    	InputStream propertiesInput = ExcelToJsonController.class.getClassLoader()
+				.getResourceAsStream("application.properties");
+    	Properties appProperties = new Properties();			
+		appProperties.load(propertiesInput);
+    	MongoClient mongoClient = new MongoClient(appProperties.getProperty("mongodb.host"),new Integer(appProperties.getProperty("mongodb.port")));
+		MongoDatabase db = mongoClient.getDatabase(appProperties.getProperty("mongodb.db"));
+
+		MongoCollection<Document> coll = db.getCollection("service_line");
+		BasicDBObject whereQuery = new BasicDBObject();
+		whereQuery.put("_id",oldId);
+		FindIterable<Document> cursor = coll.find(whereQuery);
+    	
+		if(cursor.first() != null) {
+	
+			//deleting old record
+			coll.deleteOne(Filters.eq("_id", oldId));
+			
+			//inserting new record
+			Document doc = new Document();
+			doc.append("_id", newId);
+			doc.append("description", newId);
+			coll.insertOne(doc);
+			
+			//copying old node data
+			MongoCollection<Document> nodeColl = db.getCollection(appProperties.getProperty("mongodb.collection"));
+			BasicDBObject query = new BasicDBObject();
+			query.put("_id",oldId);
+			FindIterable<Document> nodeCursor = nodeColl.find(query);
+
+			//deleting old node data
+			for (Document nc : nodeCursor) {
+				Document newDoc = new Document();
+				newDoc.append("_id", newId);
+				newDoc.append("meta",nc.get("meta"));
+				newDoc.append("data",nc.get("data"));
+				nodeColl.insertOne(newDoc);
+			}
+			
+			nodeColl.deleteOne(Filters.eq("_id", oldId));
+			
+			mongoClient.close();
+			
+			return ResponseEntity.ok().body(JSONObject.quote("Successfully renamed file : " + oldId + " to : " + newId));
+			
+		}else {
+			mongoClient.close();
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid Old Id.");
+		}
+          
+    }
+
+    
+	@CrossOrigin(origins = "*")
 	@GetMapping("/delete")
 	public String deleteData() {
 
